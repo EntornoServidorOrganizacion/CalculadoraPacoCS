bd01673971e1222fff5de35dc78fdacf29c57008
Paco Correa <43585879+pacocorreasanchez@users.noreply.github.com>
2018-10-11 13:24:02 +0200

    Organizando la aplicación

---
+++ CalculadoraPacoCS/src/main/java/es/albarregas/models/Sumador.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.albarregas.models;
+
+/**
+ *
+ * @author paco
+ */
+public class Sumador {
+    public void Sumar(int operando1, int operando2) {
+        int suma = operando1+operando2;
+    }
+}
